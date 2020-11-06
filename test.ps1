7cb3f1a5b6cdb25eb9f07af58ceb0eca795d5eff
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 14:57:06 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -1,4 +1,7 @@
 iwr https://go.microsoft.com/fwlink/?linkid=866664 -outfile mssql.exe
+
+
+
 New-Item -Path C:\SQL2017 -ItemType Directory
 $mountResult = Mount-DiskImage -ImagePath 'C:\en_sql_server_2017_enterprise_x64_dvd_11293666.iso' -PassThru
 $volumeInfo = $mountResult | Get-Volume
