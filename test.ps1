6e8204244365eeaaac2846df0f7a2302d4b591a3
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 10:19:17 +0100

    WIP

--- test.ps1
+++ test.ps1
@@ -1,1 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+[Net.ServicePointManager]::SecurityProtocol
