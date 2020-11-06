271ea1db800562d4b37da90b2cc9b98542780c43
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 15:54:01 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -2,6 +2,6 @@ iwr https://go.microsoft.com/fwlink/?linkid=866664 -outfile mssql.exe
 $cwd=get-location
 ./mssql.exe /action=download /quiet /mediapath=$cwd/mssql /mediatype=iso
 
-$r = Mount-DiskImage (get-item mssql/*.iso)
+$r = Mount-DiskImage (get-item mssql/*.iso) -passthru
 dismount-diskimage (get-item mssql/*.iso)
 
