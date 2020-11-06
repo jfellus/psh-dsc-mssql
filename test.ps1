f861f13b5b55361062f41a083e8b60c3451fd52b
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 13:10:42 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -5,3 +5,10 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name
 
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name SqlServerDsc -Force
+
+New-Item -Path C:\SQL2017 -ItemType Directory
+$mountResult = Mount-DiskImage -ImagePath 'C:\en_sql_server_2017_enterprise_x64_dvd_11293666.iso' -PassThru
+$volumeInfo = $mountResult | Get-Volume
+$driveInfo = Get-PSDrive -Name $volumeInfo.DriveLetter
+Copy-Item -Path ( Join-Path -Path $driveInfo.Root -ChildPath '*' ) -Destination C:\SQL2017\ -Recurse
+Dismount-DiskImage -ImagePath 'C:\en_sql_server_2017_enterprise_x64_dvd_11293666.iso'
