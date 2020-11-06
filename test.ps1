53e829756b692b7a42454a8a4a8d5412e406e5c5
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 15:49:53 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -2,4 +2,4 @@ iwr https://go.microsoft.com/fwlink/?linkid=866664 -outfile mssql.exe
 $cwd=get-location
 ./mssql.exe /action=download /quiet /mediapath=$cwd/mssql /mediatype=iso
 
-Mount-DiskImage -ImagePath "E:\ISO-Files\My US Visit Fall 2010 Pictures.iso"
+$r = Mount-DiskImage -ImagePath "E:\ISO-Files\My US Visit Fall 2010 Pictures.iso"
