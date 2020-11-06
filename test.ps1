6bedc2f0d3aace91b0fd39ad65bdd860033e3015
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 13:42:11 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -13,4 +13,4 @@ $mountResult = Mount-DiskImage -ImagePath 'C:\en_sql_server_2017_enterprise_x64_
 $volumeInfo = $mountResult | Get-Volume
 $driveInfo = Get-PSDrive -Name $volumeInfo.DriveLetter
 Copy-Item -Path ( Join-Path -Path $driveInfo.Root -ChildPath '*' ) -Destination C:\SQL2017\ -Recurse
-Dismount-DiskImage -ImagePath 'C:\en_sql_server_2017_enterprise_x64_dvd_11293666.iso'
+Dismount-DiskImage $mountResult.-ImagePath
