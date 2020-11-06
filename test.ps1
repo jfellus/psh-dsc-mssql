df6b76698fb2d76e6e4a88c442218c8b88da7f1d
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 15:56:14 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -3,7 +3,7 @@ $cwd=get-location
 ./mssql.exe /action=download /quiet /mediapath=$cwd/mssql /mediatype=iso
 
 $r = Mount-DiskImage (get-item mssql/*.iso) -passthru
-$vol = $r | get-volume
-$drive = Get-PSDrive -Name $vol.DriveLetter
+$vol = ($r | get-volume).DriveLetter
+$drive = Get-PSDrive -Name $vol
 dismount-diskimage (get-item mssql/*.iso)
 
