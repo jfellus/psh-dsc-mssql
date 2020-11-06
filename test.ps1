2828cb26fcdbe87133c3c7e1cf33b540ef5e5cfa
Jerome Fellus <jfellus@ct-square.com>
2020-11-06 15:50:16 +0100

    ok

--- test.ps1
+++ test.ps1
@@ -2,4 +2,5 @@ iwr https://go.microsoft.com/fwlink/?linkid=866664 -outfile mssql.exe
 $cwd=get-location
 ./mssql.exe /action=download /quiet /mediapath=$cwd/mssql /mediatype=iso
 
-$r = Mount-DiskImage -ImagePath $cwd/mssql
+$r = Mount-DiskImage -ImagePath $cwd/mssql/*.iso
+
