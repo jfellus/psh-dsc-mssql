172e6c7b3ac16576b64b1ddeecd8d2b6109a6e0a
jfellus <jfellus@gmail.com>
2020-11-10 13:19:03 +0100

    Update mssql-dump-backup.ps1

--- mssql-dump-backup.ps1
+++ mssql-dump-backup.ps1
@@ -4,7 +4,7 @@ $db = get-sqldatabase -serverinstance localhost -name db
 $db.tables.enumscript()
 $o = new-object microsoft.sqlserver.management.smo.scriptingoptions
 $o.scriptdata = $true
-$d = Get-Date -Format "yyyy-MM-dd"
+$d = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
 $db.tables.enumscript($o) | out-file c:\db_backups\$d.sql
 ls c:\db_backups\$d.sql | compress-archive -destinationpath c:\db_backups\$d.sql.zip
 rm -force c:\db_backups\$d.sql
