89249a669d0a119eaa76af4955ca45dc485841bd
jfellus <jfellus@gmail.com>
2020-11-10 09:08:58 +0100

    Update mssql-setup.ps1

--- mssql-setup.ps1
+++ mssql-setup.ps1
@@ -1,3 +1,20 @@
+# on DC as ctlabs\administrator
+
+New-ADUser -Name "SQLServer Service Account" -UserPrincipalName sql_svc `
+        -SamAccountName sql_svc `
+        -ServicePrincipalNames "SQL/iis.ctlabs.ct-square.com" `
+        -AccountPassword (convertto-securestring "Passw0rd" -asplaintext -force) `
+        -PasswordNeverExpires $True `
+        -PassThru -erroraction stop | Enable-ADAccount
+
+New-ADUser -Name "SQL Sysadmin" -UserPrincipalName sql_admin `
+        -SamAccountName sql_admin `
+        -AccountPassword (convertto-securestring "dumbPassw0rd" -asplaintext -force) `
+        -PassThru -erroraction stop | Enable-ADAccount
+
+
+# as .\administrator
+
 iwr https://go.microsoft.com/fwlink/?linkid=866664 -outfile mssql.exe
 $cwd=get-location
 ./mssql.exe /action=download /quiet /mediapath=$cwd/mssql /mediatype=iso
